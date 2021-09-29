c1eeca98dd7e6119b42cefe18c0be27fb83ea0c2
gfazioli <giovambattista.fazioli@gmail.com>
2021-09-29 18:15:30 +0200

    updates packages

--- resources/assets/jsx/components/sample.jsx
+++ resources/assets/jsx/components/sample.jsx
@@ -7,11 +7,19 @@ const View = styled.div`
 	width: 300px;
 	border-radius: 16px;
 	padding: 16px;
+
+	transition: all 0.3s ease-in-out;
+
+	&:hover {
+		box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+		transform: translateY(-4px);
+	}
 `;
 
 export const MySample = () => {
 	return (
 		<View>
+			<h1>Hello, World!</h1>
 			<h2>I'm a styled React Component</h2>
 		</View>
 	);
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,43 @@
 var path = require("path")
 
 module.exports = {
-  entry   : "./resources/assets/jsx/app.jsx",
-  output  : {
-    path : path.join(__dirname, "/public/js")
+  entry: "./resources/assets/jsx/app.jsx",
+  output: {
+    path: path.join(__dirname, "/public/js")
   },
-  resolve : {
-    alias : {
-      components : path.resolve(__dirname, "resources/assets/jsx/components")
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "resources/assets/jsx/components")
     },
     extensions: ['.js', '.jsx']
   },
-  module  : {
-    rules : [
+  module: {
+    rules: [
       {
-        test    : /\.(js|jsx)$/,
-        exclude : /node_modules/,
-        use     : {
-          loader : "babel-loader"
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
       },
       {
-        test : /\.less$/,
-        use  : [
+        test: /\.less$/,
+        use: [
           {
-            loader : "style-loader"
+            loader: "style-loader"
           },
           {
-            loader  : "css-loader",
-            options : {
-              sourceMap      : true,
-              modules        : true,
-              localIdentName : "[local]___[hash:base64:5]"
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              importLoaders: 2,
+              modules: {
+                localIdentName: "[local]___[hash:base64:5]"
+              }
             }
           },
           {
-            loader : "less-loader"
+            loader: "less-loader"
           }
         ]
       }
